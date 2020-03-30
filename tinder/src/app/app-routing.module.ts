b9703cae766b0f7e384a5b5b0d85f79943127e22
pablocastillo <pablocastillo325@gmail.com>
2020-03-30 19:16:20 -0400

    css a registro

--- tinder/src/app/app-routing.module.ts
+++ tinder/src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
   {
-    path: 'registro',
+    path: 'register',
     loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterPageModule)
   }
 ];
