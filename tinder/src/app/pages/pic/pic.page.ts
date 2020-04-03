fda0e4d1ceb91ea5e69370f97c102a7e83269e78
pablocastillo <pablocastillo325@gmail.com>
2020-04-03 07:33:05 -0400

    pic

--- tinder/src/app/pages/pic/pic.page.ts
+++ tinder/src/app/pages/pic/pic.page.ts
@@ -107,15 +107,15 @@ export class PicPage implements OnInit {
           if(this.data_img[i].id_img){
             this.ImageFirebaseService.deleteImage(this.data_img[i].path)
             this.ImageFirebaseService.deleteImageData(this.data_img[i].id_img)
-            this.utilTool.presentAlert('Exito', 'Archivo subido exitosamente', 'ok')
-            this.router.navigateByUrl('/tabs/tab1')
+            
           }
         }
       }
    
     }
 
-    
+    this.utilTool.presentAlert('Exito', 'Archivo subido exitosamente', 'ok')
+    this.router.navigateByUrl('/tabs/tab1')
 
   }
 
