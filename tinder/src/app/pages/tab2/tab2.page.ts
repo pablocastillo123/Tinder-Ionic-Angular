48e953ce2bda76fb7f4882e4af48eb4c4249ca19
Luis Villalobos <luisnazario2310@gmail.com>
2020-04-07 17:01:15 -0400

    card

--- tinder/src/app/pages/tab2/tab2.page.ts
+++ tinder/src/app/pages/tab2/tab2.page.ts
@@ -7,6 +7,27 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  people = [
+    {
+      name: 'Luis',
+      age: 21,
+      image: '../../../assets/iconoTinder.svg',
+      visible: true
+    },
+    {
+      name: 'Gustavo',
+      age: 20,
+      image: '../../../assets/kisspng-tinder-computer-icons-tinder-5b47ead292ba08.364455671531439826601.svg',
+      visible: true
+    },
+    {
+      name: 'Pablo',
+      age: 20,
+      image: '../../../assets/c907389646e48bba09604c9d67f9edd3.jpg',
+      visible: true
+    }
+  ]
+
 
   constructor() {
   }
