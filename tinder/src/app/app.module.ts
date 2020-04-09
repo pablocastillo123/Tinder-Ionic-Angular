83b4a54b3c3f018dd1600cba2413da1e07374227
pablocastillo <pablocastillo325@gmail.com>
2020-04-09 16:40:56 -0400

    1er pasos para las notificaciones con FCM
    notificaciones  con FCM

--- tinder/src/app/app.module.ts
+++ tinder/src/app/app.module.ts
@@ -16,7 +16,7 @@ import {AngularFireStorageModule} from '@angular/fire/storage';
 import {environment} from './../environments/environment';
 
 import { Camera } from '@ionic-native/camera/ngx';
-
+import { FCM } from '@ionic-native/fcm/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -31,7 +31,7 @@ import { Camera } from '@ionic-native/camera/ngx';
   providers: [
     StatusBar,Camera,
     SplashScreen,
-    Camera,
+    Camera,FCM,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
   ],
   bootstrap: [AppComponent]
--- tinder/src/app/pages/tab2/tab2.page.ts
+++ tinder/src/app/pages/tab2/tab2.page.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
+import { UtilToolService } from './../../services/utiltool.service';
+import { Component, OnInit } from '@angular/core';
+import { FCM } from '@ionic-native/fcm/ngx';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit{
 
 
-  constructor() {
+  constructor(private UtilToolService:UtilToolService, private fcm:FCM ) {
   }
-
-
+  ngOnInit(){
+    this.fcm.getToken().then(token => {
+      this.UtilToolService.presentAlert('msg',token,'ok');
+    }).catch(err =>{
+      this.UtilToolService.presentAlert('msg',err,'ok');
+      console.log(err)
+    })
+  }
+  
 
 }
